ba66dbcfa3aa4a235e633a578eb7977c859fa364
s-illly <lilyapples696@gmail.com>
2025-07-04 22:55:38 -0400

    Incomplete header

--- hack404/src/App.tsx
+++ hack404/src/App.tsx
@@ -5,10 +5,14 @@ function App() {
 
   return (
     <>
-    <header className="bg-white h-30 w-full flex ">
-      <nav  >
-        <div className="flex lg:flex-1">
-
+    <header className="bg-white h-30 w-full flex top-0">
+      <nav className="max-w-7xl w-full">
+        <div className="flex pl-4 pt-2 pb-2 text-black text-left">
+          Hack404
+        </div>
+        <hr className="border-gray-800 max-w-7xl w-full items-center" />
+        <div className="flex pl-4 pt-2 pb-2 text-black text-left">
+          
         </div>
       </nav>
     </header>
