50b8dcd6da5821839e8e906c82f28c03d32a5ad2
s-illly <lilyapples696@gmail.com>
2025-07-05 12:00:29 -0400

    no more services/api.ts

---
+++ hack404/src/components/ornament.tsx
@@ -0,0 +1,9 @@
+const Ornament = () => {
+    return (
+        <div className="bg-white rounded-3xl shadow-md p-3 self-start sticky top-[160px] z-10 [w-50px]">
+
+        </div>
+    )
+}
+
+export default Ornament 
