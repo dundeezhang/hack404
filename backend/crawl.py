30f4a9b46ef9f76400085a38ceb1421faffdfe4d
dundeezhang <dundee_zhang@hotmail.com>
2025-07-05 00:28:06 -0400

    headless mode

--- backend/crawl.py
+++ backend/crawl.py
@@ -1,13 +1,16 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+driver = webdriver.Chrome(options=chrome_options)
 
 def crawl_page(url):
-    driver = webdriver.Chrome()
-    
     try:
         driver.get(url)
         
-        # Get the body element content
+        # get the body content
         body_element = driver.find_element(By.TAG_NAME, "body")
         body_content = body_element.text
         
