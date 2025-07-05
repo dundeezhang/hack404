1b570be92f0c4a69af9b56a4d4648d8b579c564b
s-illly <lilyapples696@gmail.com>
2025-07-05 11:11:48 -0400

    animation

--- hack404/src/components/gradient.tsx
+++ hack404/src/components/gradient.tsx
@@ -1,11 +1,11 @@
 const Gradient = () => {
     return (
         <div className="grid grid-cols-5 h-10 w-full rounded-lg mb-4">
-            <div className="w-full h-full bg-[#B8E2DC]" />
-            <div className="w-full h-full bg-[#B8E2D4]" />
-            <div className="w-full h-full bg-[#B8E2CD]" />
-            <div className="w-full h-full bg-[#B8E2C6]" />
-            <div className="w-full h-full bg-[#B8E2B9]" />
+            <div className="w-full h-full bg-[#B8E2DC] animate-bounce delay-5" />
+            <div className="w-full h-full bg-[#B8E2D4] animate-bounce delay-10" />
+            <div className="w-full h-full bg-[#B8E2CD] animate-bounce delay-15" />
+            <div className="w-full h-full bg-[#B8E2C6] animate-bounce delay-20" />
+            <div className="w-full h-full bg-[#B8E2B9] animate-bounce delay-25" />
         </div>
     )
 }
