c0136bce73ef06c2f067f2954084195871f36c4d
daynaaaa <danya.eggy@gmail.com>
2025-07-05 03:20:36 -0400

    changed navbar a bit

--- hack404/src/App.tsx
+++ hack404/src/App.tsx
@@ -1,49 +1,19 @@
+import { useState } from "react";
 import "./App.css";
+import Header from "./components/Header";
 import HenYang from "./components/HenYang";
 import NewsCard from "./components/NewsCard";
 import Gradient from "./components/gradient";
 
 function App() {
+  const [active, setActive] = useState("");
   return (
     <>
       <header
         className="bg-white h-fit w-full flex top-0 sticky shadow-md z-9999"
         style={{ fontFamily: "AlumniSans" }}
       >
-        <nav className="w-full">
-          <div className="flex pl-4 pt-2 pb-2 text-black text-xl ">Hack404</div>
-          <hr className="border-gray-300 w-full items-center" />
-          <div className="flex gap-3 pl-4 pt-2 pb-2 text-black text-left">
-            <div className="my-auto">
-              <img src="/src/assets/yang.png" className="h-12 w-12 mr-2" />
-            </div>
-            <div className="grid grid-rows-2 gap-0.1">
-              <div className="font-bold text-3xl">YangNews</div>
-              <p className="text-lg">Find Yang daily</p>
-            </div>
-
-            <div className="ml-8 flex items-center space-x-8 text-xl">
-              <a href="#" className="text-gray-900 ">
-                Politics
-              </a>
-              <a href="#" className="text-gray-900 ">
-                Environment
-              </a>
-              <a href="#" className="text-gray-900 ">
-                Community
-              </a>
-              <a href="#" className="text-gray-900 ">
-                Science
-              </a>
-              <a href="#" className="text-gray-900 ">
-                Health
-              </a>
-              <a href="#" className="text-gray-900 ">
-                Education
-              </a>
-            </div>
-          </div>
-        </nav>
+        <Header active={active} setActive={setActive} />
       </header>
       <main className="p-12">
         <Gradient />
--- hack404/src/components/NewsCard.tsx
+++ hack404/src/components/NewsCard.tsx
@@ -4,77 +4,79 @@ import { faClock } from "@fortawesome/free-solid-svg-icons";
 
 const NewsCard = ({ article }: { article?: Article }) => {
   return (
-    <div
-      className="relative bg-white w-[300px] rounded-3xl shadow-md text-xl"
-      style={{ fontFamily: "AlumniSans" }}
-    >
-      <div className="relative w-[300px] h-[250px]">
-        <div className="absolute w-full h-full bg-[#D9D9D9] rounded-t-3xl z-0 items-center" />
-        {/* Default Image */}
-        <img
-          src="/src/assets/fillerimg.png"
-          className="mx-auto absolute w-full h-full object-cover rounded-t-3xl z-10"
-          alt="Default"
-        />
-        <div className="absolute bottom-0 w-full z-20 bg-gradient-to-t from-black/70 to-transparent px-4 py-2">
-          <h1
-            className="text-white text-3xl"
-            style={{ fontFamily: "AlumniSans", fontWeight: 700 }}
-          >
-            Title title title title title
-          </h1>
+    <a href="https://www.google.com" target="_blank" rel="noopener noreferrer">
+      <div
+        className="relative bg-white w-[300px] rounded-3xl shadow-md text-xl"
+        style={{ fontFamily: "AlumniSans" }}
+      >
+        <div className="relative w-[300px] h-[250px]">
+          <div className="absolute w-full h-full bg-[#D9D9D9] rounded-t-3xl z-0 items-center" />
+          {/* Default Image */}
+          <img
+            src="/src/assets/fillerimg.png"
+            className="mx-auto absolute w-full h-full object-cover rounded-t-3xl z-10"
+            alt="Default"
+          />
+          <div className="absolute bottom-0 w-full z-20 bg-gradient-to-t from-black/70 to-transparent px-4 py-2">
+            <h1
+              className="text-white text-3xl"
+              style={{ fontFamily: "AlumniSans", fontWeight: 700 }}
+            >
+              Title title title title title
+            </h1>
+          </div>
         </div>
-      </div>
 
-      <div className="p-3">
-        <div className="flex flex-row justify-between">
-          <div className="flex flex-row gap-3 h-[50] my-auto">
-            <div className="my-auto h-[40px] w-[40px] rounded-full bg-[#22333B]" />
-            <h2
-              className="my-auto text-[#22333B]"
-              style={{ fontFamily: "AlumniSans", fontWeight: 600 }}
+        <div className="p-3">
+          <div className="flex flex-row justify-between">
+            <div className="flex flex-row gap-3 h-[50] my-auto">
+              <div className="my-auto h-[40px] w-[40px] rounded-full bg-[#22333B]" />
+              <h2
+                className="my-auto text-[#22333B]"
+                style={{ fontFamily: "AlumniSans", fontWeight: 600 }}
+              >
+                First Last
+              </h2>
+            </div>
+            {/* <div className="flex flex-row justify-between"> */}
+            <div className="flex flex-row gap-2 w-1/2 justify-end">
+              <FontAwesomeIcon
+                className="my-auto"
+                icon={faClock}
+                color="#8A8A8A"
+              />
+              <p
+                className="text-[#8A8A8A] my-auto text-sm"
+                style={{ fontFamily: "AlumniSans", fontWeight: 300 }}
+              >
+                8 mins ago
+              </p>
+            </div>
+          </div>
+          {/* </div> */}
+          <div className="mt-5 flex flex-row flex-wrap gap-3">
+            <div
+              className="rounded-xl border border-[#A9927D] px-2 py-1"
+              style={{ fontFamily: "AlumniSans", fontWeight: 500 }}
             >
-              First Last
-            </h2>
-          </div>
-          {/* <div className="flex flex-row justify-between"> */}
-          <div className="flex flex-row gap-3 w-1/2 justify-end">
-            <FontAwesomeIcon
-              className="my-auto"
-              icon={faClock}
-              color="#22333B"
-            />
-            <p
-              className="text-[#22333B] my-auto text-sm"
-              style={{ fontFamily: "AlumniSans", fontWeight: 300 }}
+              <p className="text-[#A9927D] text-sm">sports</p>
+            </div>
+            <div
+              className="rounded-xl border border-[#A9927D] px-2 py-1"
+              style={{ fontFamily: "AlumniSans", fontWeight: 500 }}
             >
-              8 mins ago
-            </p>
-          </div>
-        </div>
-        {/* </div> */}
-        <div className="mt-5 flex flex-row flex-wrap gap-3">
-          <div
-            className="rounded-xl border border-[#A9927D] px-2 py-1"
-            style={{ fontFamily: "AlumniSans", fontWeight: 500 }}
-          >
-            <p className="text-[#A9927D] text-sm">sports</p>
-          </div>
-          <div
-            className="rounded-xl border border-[#A9927D] px-2 py-1"
-            style={{ fontFamily: "AlumniSans", fontWeight: 500 }}
-          >
-            <p className="text-[#A9927D] text-sm">sports</p>
-          </div>
-          <div
-            className="rounded-xl border border-[#A9927D] px-2 py-1"
-            style={{ fontFamily: "AlumniSans", fontWeight: 500 }}
-          >
-            <p className="text-[#A9927D] text-sm">sports</p>
+              <p className="text-[#A9927D] text-sm">sports</p>
+            </div>
+            <div
+              className="rounded-xl border border-[#A9927D] px-2 py-1"
+              style={{ fontFamily: "AlumniSans", fontWeight: 500 }}
+            >
+              <p className="text-[#A9927D] text-sm">sports</p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </a>
   );
 };
 
